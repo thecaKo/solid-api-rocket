0844b4128dd81a98dde8996369de0196ff9effc4
cako <cakoonrails@gmail.com>
2025-02-19 16:25:01 -0300

    adding repositories patterns

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,9 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
+import { register } from "./http/controllers/register";
+import { appRoutes } from "./http/routes";
+
 export const app = fastify();
+
+app.register(appRoutes);
---
+++ src/use-cases/register.ts
@@ -0,0 +1,33 @@
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+import { PrismaUserRepository } from "@/repositories/prisma-users-repository";
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUseCaseRequest) {
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (userWithSameEmail) {
+    throw new Error("E-mail alredy exists.");
+  }
+
+  const prismaUsersRepository = new PrismaUserRepository();
+
+  prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
+  });
+}
