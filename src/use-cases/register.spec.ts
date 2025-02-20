e78a08652b97c154197d839140b24d821f7835a9
cako <cakoonrails@gmail.com>
2025-02-20 10:38:44 -0300

    adding first tests suits

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,57 @@
-import { expect, test } from "vitest";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { expect, describe, it } from "vitest";
+import { RegisterUseCase } from "./register";
+import { compare } from "bcryptjs";
+import { UserAlredyExistsError } from "./errors/user-alredy-exists-error";
 
-test("AAA", () => {
-  expect(2 + 2).toBe(4);
+describe("Register Use Case", () => {
+  it("should register a new user", async () => {
+    const userRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(userRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "Fulano",
+      email: "Fulano@gmail.com",
+      password: "123456",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it("should hash user password upon registration", async () => {
+    const userRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(userRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "Fulano",
+      email: "Fulano@gmail.com",
+      password: "123456",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      user.password_hash,
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+
+  it("should not be able to register with same email twice", async () => {
+    const userRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(userRepository);
+
+    await registerUseCase.execute({
+      name: "Fulano",
+      email: "Fulano@gmail.com",
+      password: "123456",
+    });
+
+    await expect(() =>
+      registerUseCase.execute({
+        name: "Fulano",
+        email: "Fulano@gmail.com",
+        password: "123456",
+      }),
+    ).rejects.toBeInstanceOf(UserAlredyExistsError);
+  });
 });
