55e8b9c7113a53f40fbc4cc5657346508c04d972
cako <cakoonrails@gmail.com>
2025-02-26 18:05:35 -0300

    adding prisma repositories

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,38 @@
+import { Gym, Prisma } from "@prisma/client";
+import { FindManyNearbyParams, GymsRepository } from "../gyms-repository";
+import { prisma } from "@/lib/prisma";
+
+export class PrismaGymRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: { id },
+    });
+
+    return gym;
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: { title: { contains: query } },
+      skip: (page - 1) * 20,
+      take: 20,
+    });
+
+    return gyms;
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `;
+    return gyms;
+  }
+  async create(data: Prisma.GymCreateInput): Promise<Gym> {
+    const gym = await prisma.gym.create({
+      data,
+    });
+
+    return gym;
+  }
+}
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,17 @@
 import { prisma } from "@/lib/prisma";
-import { Prisma } from "@prisma/client";
+import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
 export class PrismaUserRepository implements UsersRepository {
+  async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: { email },
