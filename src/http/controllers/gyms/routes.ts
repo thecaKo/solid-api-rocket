e20f41dd8ed43542532a7f8061f2c3e3edbdac89
cako <cakoonrails@gmail.com>
2025-02-28 16:28:49 -0300

    adding routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
+import { search } from "./search";
+import { nearbt } from "./nearby";
+import { create } from "./create";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
+
+  app.get("/gyms/search", search);
+  app.get("/gyms/nearbt", nearbt);
+
+  app.post("/gyms", create);
 }
